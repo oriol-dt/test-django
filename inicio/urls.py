951e8e21780b9d89d7ce7f05b3120a4fbe878e23
Oriol <orioldu@hotmail.com>
2026-02-15 00:28:36 +0100

    App finalizada

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from . import views
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    path('completar/<int:tarea_id>/', views.completar_tarea, name='completar_tarea'),
+    path('eliminar/<int:tarea_id>/', views.eliminar_tarea, name='eliminar_tarea'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import TareaForm
 from .models import Tarea
 
@@ -17,3 +17,14 @@ def home(request):
         'tareas': tareas,
         'form': form
     })
+
+def completar_tarea(request, tarea_id):
+    tarea = get_object_or_404(Tarea, id=tarea_id)
+    tarea.completada = not tarea.completada
+    tarea.save()
+    return redirect('home')
+
+def eliminar_tarea(request, tarea_id):
+    tarea = get_object_or_404(Tarea, id=tarea_id)
+    tarea.delete()
+    return redirect('home')
