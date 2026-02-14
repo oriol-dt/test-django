ff9fd31937a3d068fc7519e1b903f9b8c674c5f8
Oriol <orioldu@hotmail.com>
2026-02-14 02:26:57 +0100

    Proyecto funcional

--- inicio/views.py
+++ inicio/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import TareaForm
 from .models import Tarea
 
 def home(request):
+    if request.method == 'POST':
+        form = TareaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TareaForm()
+
     tareas = Tarea.objects.all()
 
-    return render(request, 'inicio/lista_tareas.html', {'tareas': tareas})
+    return render(request, 'inicio/lista_tareas.html', {
+        'tareas': tareas,
+        'form': form
+    })
